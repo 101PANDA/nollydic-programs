de1da8c27926f1aa1368ca7309ad44214a4c4c23
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-29 20:50:07 +0100

    Instructions

---
+++ nollydic_logbook/bookApp/__init__.py
@@ -0,0 +1,7 @@
+"""
+Run the app in the terminal with the commands
+$ cd <Main project directory>
+$ python3  nollydic_logbook/bookApp/main.py
+
+then test it on the web address: localhost:5000
+"""
