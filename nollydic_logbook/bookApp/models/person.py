fc32db547a445da8b77be8c9c472ee6aa87fafaa
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 11:06:07 +0100

    Create models

---
+++ nollydic_logbook/bookApp/models/person.py
@@ -0,0 +1,42 @@
+"""Person Model
+
+"""
+from uuid import uuid4
+import models
+
+
+class Person:
+
+    name = ""
+    datetime = None
+    role = ""
+    department = ""
+    phone = ""
+
+    def __init__(self, **kwargs):
+        if kwargs:
+            self.update(**kwargs)
+        else:
+            self.id = str(uuid4())
+            models.storage.add(self)
+
+
+    def to_dict(self):
+        attrs = self.__dict__.copy()
+        return attrs
+
+    def update(self, **kwargs):
+        if "id" in kwargs:
+            self.__dict__.update('id', kwargs["id"])
+        if "name" in kwargs:
+            self.__dict__.update("name", kwargs["name"])
+        if "datetime" in kwargs:
+            self.__dict__.update("datetime", kwargs["datetime"])
+        if "role" in kwargs:
+            self.__dict__.update("role", kwargs["role"])
+        if "department" in kwargs:
+            self.__dict__.update("department", kwargs["department"])
+        if "phone" in kwargs:
+            self.__dict__.update("phone", kwargs["phone"])
+
+        models.storage.save()
---
+++ nollydic_logbook/bookApp/models/storages/file_storage.py
@@ -0,0 +1,43 @@
+"""FileStorage Model
+
+"""
+import json
+from ..person import Person
+
+class FileStorage:
+
+    __objects = {}
+    __filepath = "nollybook.json"
+
+    def add(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def remove(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        if key in self.__objects:
+            del self.__objects[key]
+
+    def save(self):
+        json_data = {}
+        self.load()
+
+        for key, value in self.__objects.items():
+            value = value.to_dict()
+            json_data[key] = value
+
+        with open(self.__filepath, "w", encoding="utf8") as file:
+            file.write(json.dumps(json_data))
+
+    def load(self):
+        json_data = {}
+
+        try:
+            with open(self.__filepath, "r", encoding="utf8") as file:
+                json_data = json.loads(file.read())
+        except IOError:
+            pass
+        else:
+            for key, value in json_data.items():
+                value = Person(**value)
+                self.__objects.update(key, value)
