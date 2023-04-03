3e53913f281cf5ec906a8ba29324d96b1c37aeef
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-03 13:46:42 +0100

    Add dynamic clicks

--- nollydic_logbook/bookApp/main.py
+++ nollydic_logbook/bookApp/main.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template, request
-from models.person import Person
 from datetime import datetime
+from flask import render_template, request, redirect, url_for
+from models.person import Person
+from models import storage
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def home():
 
         name = form.get("fullName")
         date = datetime.now().isoformat()
+        in_date = form.get("date")
         role = form.get("position")
         if role == "Staff":
             role_info = form.get("department")
@@ -20,8 +22,20 @@ def home():
         phone = form.get("telNo")
 
         person = Person()
-        person.update(name=name, datetime=date, role=role, department=role_info, phone=phone)
+        person.update(name=name, datetime=date, role=role, role_info=role_info, phone=phone)
     return render_template("mainform.html")
+
+@app.route("/admin", methods=["GET"])
+def admin():
+    logged_p = storage.today()
+    visitor = []
+    staff = []
+    for person in logged_p:
+        if person.role == "Staff":
+            staff.append(person)
+        elif person.role == "Visitor":
+            visitor.append(person)
+    return render_template("mbook_sheet.html", staffs=staff, visitors=visitor)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", "5000")
